6c5d2f396b63327672a01211ce087520c732dbe6
HafseeMan <hafoosah1@gmail.com>
2020-12-07 20:57:12 +0100

    updates to backend.py

--- backEnd.py
+++ backEnd.py
@@ -1,10 +1,11 @@
 import sqlite3
+import re
 
 def connect():
     '''connect to database or creates if not existing'''
     conn = sqlite3.connect('Students.db')
     print("Opened student database successfully")
-
+    #cursor for ..?
     cur = conn.cursor()
     cur.execute('''
         CREATE TABLE IF NOT EXISTS Students
@@ -17,7 +18,7 @@ def connect():
             USERNAME TEXT);
     ''')
     print ("Table created successfully")
-    conn.commit() 
+    conn.commit()
     conn.close()
     print("Opened database successfully")
 
@@ -32,10 +33,18 @@ def add(reg,fn,ln,age,email,course,user):
     '''
     conn = sqlite3.connect("Students.db")
     print("Database connected")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO Students Values (?,?,?,?,?,?,?)",(reg,fn,ln,age,email,course,user))
-    conn.commit()
-    print("Record successfully added")
+
+    #validating email. Entry only accepted if email is valid
+    regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w{2,3}$'
+    if(re.search(regex,email)):
+        cur = conn.cursor()
+        cur.execute("INSERT INTO Students Values (?,?,?,?,?,?,?)", (reg, fn, ln, age, email, course, user))
+        conn.commit()
+        print("Record successfully added")
+
+    else:
+        print("INVALID EMAIL")
+        print("Adding Record Failed")
     conn.close()
 
 def display():
@@ -43,32 +52,75 @@ def display():
     conn = sqlite3.connect('Students.db')
     print("Connected to database")
 
-    cursor = conn.execute("SELECT REG, FNAME, LNAME, AGE, EMAIL, COURSE, USERNAME from Students")
-    for row in cursor:
-        print("REG. NUMBER = " + str(row[0]))
-        print("NAME = " + str(row[1]) + " " + str(row[2]))
-        print("AGE = " + str(row[3]))
-        print("EMAIL = " + str(row[4]))
-        print("COURSE = " + str(row[5]))
-        print("____________________________________________")
-    print("Display operation successful")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Students")
+    rows = cur.fetchall()
+
+    for row in rows:
+        print("REGISTRATION NUMBER = " + str(row[0]))
+        print("NAME = " + str(row[1])+" "+str(row[2]))
+        print("======================================")
+
     conn.close()
 
-def update(reg):
-    print("hey baby!!")
+def update(reg,targetAttribute,newValue):
+    #find data of student with Registration no. = reg, and change the "targetAttribute" to the "newValue"
+    conn = sqlite3.connect("Student.db")
+    print("Connected to database")
+    cur = conn.cursor()
+    if (targetAttribute == "FNAME"):
+        cur.execute('UPDATE Students SET FNAME=? WHERE REG= ?',(newValue, reg))
+        conn.commit()
+        display()
+    elif (targetAttribute == "LNAME"):
+        cur.execute('UPDATE Students SET LNAME=? WHERE REG= ?', (newValue, reg))
+        conn.commit()
+        display()
+    elif (targetAttribute == "AGE"):
+        cur.execute('UPDATE Students SET AGE=? WHERE REG= ?', (newValue, reg))
+        conn.commit()
+        display()
+    elif(targetAttribute == "EMAIL"):
+        cur.execute('UPDATE Students SET EMAIL=? WHERE REG= ?', (newValue, reg))
+        conn.commit()
+        display()
+    elif (targetAttribute == "COURSE"):
+        cur.execute('UPDATE Students SET COURSE=? WHERE REG= ?', (newValue, reg))
+        conn.commit()
+        display()
+    elif (targetAttribute == "USER"):
+        cur.execute('UPDATE Students SET USER=? WHERE REG= ?', (newValue, reg))
+        conn.commit()
+        display()
+    else:
+        print("wrong input.")
 
 def delete(reg):
     '''function to delete student from database'''
     conn = sqlite3.connect("Students.db")
     print("Connected to database")
 
-    conn.execute("DELETE from Students where REG=?",(reg))
+    cur = conn.cursor()
+    cur.execute("DELETE FROM Students WHERE REG=?",(reg,))
     conn.commit()
     print("Student "+reg+"  was deleted")
 
     print("Delete Operation successful")
     conn.close()
 
-connect()
-#add("1103","dss",'dsd',23,"sdsa","dsds","dsddd")
-display()
+#========================================================================
+
+#TESTING
+connect() #works
+
+add("ABC123","mONSTER","APPLE",100,"FNKJNJ","NKNK","NKNK") #works
+
+display() #works
+
+update("U17CS1104","FNAME","BABYyyyyy") #error
+"""ERROR: cur.execute('UPDATE Students SET FNAME=? WHERE REG= ?',(newValue, reg))
+sqlite3.OperationalError: no such table: Students"""
+
+delete("U17CS1104") #error
+"""ERROR: cur.execute("DELETE FROM Students WHERE REG=?",(reg,))
+sqlite3.OperationalError: no such column: REG"""
